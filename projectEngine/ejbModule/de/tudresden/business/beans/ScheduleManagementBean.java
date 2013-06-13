60b5d2337321a59c062fe4037e6bd4141bd29263
Arjun Naik <arjun.rn@gmail.com>
2013-06-13 19:56:38 +0200

    Changes to Schedule Management Bean

--- projectEngine/ejbModule/de/tudresden/business/beans/ScheduleManagementBean.java
+++ projectEngine/ejbModule/de/tudresden/business/beans/ScheduleManagementBean.java
@@ -1,6 +1,6 @@
 package de.tudresden.business.beans;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.Remote;
 
@@ -8,10 +8,12 @@ import de.tudresden.business.businessobjects.Appointment;
 import de.tudresden.business.businessobjects.User;
 
 @Remote
-public interface ScheduleManagementBean
-{
+public interface ScheduleManagementBean {
 	public void createUserAppointment(User user, Appointment appointment);
+
 	public void deleteUserAppointment(User user, Appointment appointment);
+
 	public void addUserInAppointment(User user, Appointment appointment);
-	public ArrayList<Appointment> getAllUserAppointments(User user);
+
+	public List<Appointment> getAllUserAppointments(User user);
 }
--- projectEngine/ejbModule/de/tudresden/business/beans/ScheduleManagementBeanImpl.java
+++ projectEngine/ejbModule/de/tudresden/business/beans/ScheduleManagementBeanImpl.java
@@ -1,43 +1,70 @@
 package de.tudresden.business.beans;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import de.tudresden.business.businessobjects.Appointment;
+import de.tudresden.business.businessobjects.Schedule;
 import de.tudresden.business.businessobjects.User;
 
+/**
+ * This class is an implementation of the {@link ScheduleManagementBean}
+ * interface.
+ * 
+ * @author Arjun Naik, Rodrigo Lins de Oliveira
+ * 
+ */
 @Stateful
-public class ScheduleManagementBeanImpl implements ScheduleManagementBean
-{
+public class ScheduleManagementBeanImpl implements ScheduleManagementBean {
 	@PersistenceContext
 	private EntityManager em;
 
+	/**
+	 * Used to add an appointment to a {@link User}'s schedule.
+	 * 
+	 * @param user
+	 *            : The user to whose {@link Schedule} the appointment has to be
+	 *            added.
+	 * @param appointment
+	 *            : The {@link Appointment} to be added to the user's schedule.
+	 */
 	public void createUserAppointment(User user, Appointment appointment) {
-		// TODO Auto-generated method stub
-
+		Schedule userSchedule = user.getSchedule();
+		userSchedule.addAppointment(appointment);
+		System.out.println("Added new appointment to user schedule");
+		em.persist(userSchedule);
+		System.out.println("Sucessfully persisted the new appointment");
 	}
 
 	public void addUserInAppointment(User user, Appointment appointment) {
-		// TODO Auto-generated method stub
-
+		createUserAppointment(user, appointment);
 	}
 
-	public ArrayList<Appointment> getAllUserAppointments(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void helloWorld(String helloWorld)
-	{
-		System.out.println("ScheduleManagementBeanImpl says: " + helloWorld);
+	/**
+	 * This functions returns all the {@link Appointment}s for a {@link User}
+	 * 
+	 * @param user
+	 *            : The {@link User}
+	 * @return All the user's {@link Appointment}s
+	 */
+	public List<Appointment> getAllUserAppointments(User user) {
+		return user.getSchedule().getAppointments();
 	}
 
 	@Override
 	public void deleteUserAppointment(User user, Appointment appointment) {
-		
+		List<Appointment> appointments = user.getSchedule().getAppointments();
+		System.out.println("Appointments for User retrived: "
+				+ appointments.size());
+		appointments.remove(appointment);
+		System.out.println("Removed Appointment");
+		user.getSchedule().setAppointments(appointments);
+		System.out.println("Attempting to persist changes to schedule");
+		em.persist(user.getSchedule());
+		System.out.println("Persisted changes to User schedule");
 	}
 
 }
